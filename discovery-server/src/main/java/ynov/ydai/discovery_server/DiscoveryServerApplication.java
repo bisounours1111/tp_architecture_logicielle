6a26d9067c211bf8df173d24821b28ea43dbe35b
Bisounours <yan.dai@outlook.fr>
2026-03-19 10:34:50 +0100

    first commit

---
+++ discovery-server/src/main/java/ynov/ydai/discovery_server/DiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package ynov.ydai.discovery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer; // Ajoute cet import
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+
+}
